07d6edf879435e4c199535e12c985d6539bde665
TylerGoeringer <tyler.goeringer@gmail.com>
2022-08-20 18:52:17 -0700

    Move patches for SettingsEntityImage into the class

--- ModMenu/Settings/TestSettings.cs
+++ ModMenu/Settings/TestSettings.cs
@@ -1,21 +1,9 @@
-﻿using HarmonyLib;
-using Kingmaker.UI.MVVM._PCView.Settings;
-using Kingmaker.UI.MVVM._VM.Settings;
-using Kingmaker.UI.MVVM._VM.Settings.Entities.Decorative;
-using Kingmaker.UI.MVVM._VM.Settings.Entities.Difficulty;
-using Kingmaker.UI.MVVM._VM.Settings.Entities;
-using Kingmaker.UI.SettingsUI;
-using ModMenu.NewTypes;
-using Owlcat.Runtime.UI.MVVM;
-using Owlcat.Runtime.UI.VirtualListSystem;
+﻿using Kingmaker.UI.SettingsUI;
 using System.IO;
 using UnityEngine;
 
 namespace ModMenu.Settings
 {
-  // Note: Settings keys cannot have uppercase letters
-  // Note 2: Only types that work are in GetVMForSettingsItem
-
   /// <summary>
   /// Test settings group shown on debug builds to validate usage.
   /// </summary>
@@ -95,50 +83,6 @@ namespace ModMenu.Settings
       _ = texture.LoadImage(bytes);
       var sprite = Sprite.Create(texture, new Rect(0, 0, size, size), new Vector2(0, 0));
       return sprite;
-    }
-
-    [HarmonyPatch(typeof(SettingsVM))]
-    static class SettingsVM_Patch
-    {
-      [HarmonyPatch(nameof(SettingsVM.GetVMForSettingsItem)), HarmonyPrefix]
-      static bool Prefix(UISettingsEntityBase uiSettingsEntity, ref VirtualListElementVMBase __result)
-      {
-        if (uiSettingsEntity is UISettingsEntityImage imageEntity)
-        {
-          Main.Logger.Log("Returning image VM");
-          __result = new SettingsEntityImageVM(imageEntity);
-          return false;
-        }
-        return true;
-      }
-    }
-
-    [HarmonyPatch(typeof(SettingsPCView.SettingsViews))]
-    static class SettingsViews_Patch
-    {
-      [HarmonyPatch(nameof(SettingsPCView.SettingsViews.InitializeVirtualList)), HarmonyPrefix]
-      static bool Prefix(SettingsPCView.SettingsViews __instance, VirtualListComponent virtualListComponent)
-      {
-        Main.Logger.NativeLog("Initializing image view.");
-        var obj = new GameObject("ImageView", typeof(RectTransform));
-        var prefabToAddtoList = obj.AddComponent<SettingsEntityImageView>();
- 
-        virtualListComponent.Initialize(new IVirtualListElementTemplate[]
-        {
-          new VirtualListElementTemplate<SettingsEntityHeaderVM>(__instance.m_SettingsEntityHeaderViewPrefab),
-          new VirtualListElementTemplate<SettingsEntityBoolVM>(__instance.m_SettingsEntityBoolViewPrefab),
-          new VirtualListElementTemplate<SettingsEntityDropdownVM>(__instance.m_SettingsEntityDropdownViewPrefab, 0),
-          new VirtualListElementTemplate<SettingsEntitySliderVM>(__instance.m_SettingsEntitySliderViewPrefab, 0),
-          new VirtualListElementTemplate<SettingEntityKeyBindingVM>(__instance.m_SettingEntityKeyBindingViewPrefab),
-          new VirtualListElementTemplate<SettingsEntityDropdownVM>(__instance.m_SettingsEntityDropdownDisplayModeViewPrefab, 1),
-          new VirtualListElementTemplate<SettingsEntityDropdownGameDifficultyVM>(__instance.m_SettingsEntityDropdownGameDifficultyViewPrefab, 0),
-          new VirtualListElementTemplate<SettingsEntitySliderVM>(__instance.m_SettingsEntitySliderVisualPerceptionViewPrefab, 1),
-          new VirtualListElementTemplate<SettingsEntitySliderVM>(__instance.m_SettingsEntitySliderVisualPerceptionWithImagesViewPrefab, 2),
-          new VirtualListElementTemplate<SettingsEntityStatisticsOptOutVM>(__instance.m_SettingsEntityStatisticsOptOutViewPrefab),
-          new VirtualListElementTemplate<SettingsEntityImageVM>(prefabToAddtoList),
-        });
-        return false;
-      }
     }
   }
 }
