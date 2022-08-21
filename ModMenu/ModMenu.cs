d1bb2a2afaeab5b24993b358d766384f4f64eb3f
TylerGoeringer <tyler.goeringer@gmail.com>
2022-08-20 18:11:39 -0700

    Add main API entry point

---
+++ ModMenu/ModMenu.cs
@@ -0,0 +1,32 @@
+﻿using Kingmaker.UI.SettingsUI;
+using ModMenu.Settings;
+
+namespace ModMenu
+{
+  /// <summary>
+  /// API mods can use to add settings to the Mods menu page.
+  /// </summary>
+  public static class ModMenu
+  {
+    /// <summary>
+    /// Adds a new group of settings to the Mods menu page.
+    /// </summary>
+    public static void AddSettings(SettingsGroup settingsGroup)
+    {
+      ModsMenuEntity.ModSettings.Add(settingsGroup.Build());
+    }
+
+    /// <summary>
+    /// Adds a new group of settings to the Mods menu page.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// Using <see cref="AddSettings(SettingsGroup)"/> is recommended. If you prefer to construct the settings on your
+    /// own you can use this method.
+    /// </remarks>
+    public static void AddSettings(UISettingsGroup settingsGroup)
+    {
+      ModsMenuEntity.ModSettings.Add(settingsGroup);
+    }
+  }
+}
