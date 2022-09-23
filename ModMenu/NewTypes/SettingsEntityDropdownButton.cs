0128dc9c71d1f5975e3be20a852f86fbccc4b49c
TylerGoeringer <tyler.goeringer@gmail.com>
2022-09-23 08:59:48 -0700

    Create SettingsEntityDropdownButton.cs

---
+++ ModMenu/NewTypes/SettingsEntityDropdownButton.cs
@@ -0,0 +1,126 @@
+﻿using Kingmaker.Localization;
+using Kingmaker.PubSubSystem;
+using Kingmaker.UI.MVVM._PCView.Settings.Entities;
+using Kingmaker.UI.MVVM._VM.Settings.Entities;
+using Kingmaker.UI.SettingsUI;
+using Owlcat.Runtime.UI.Controls.Button;
+using Owlcat.Runtime.UI.VirtualListSystem.ElementSettings;
+using System;
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ModMenu.NewTypes
+{
+  public class UISettingsEntityDropdownButton : UISettingsEntityDropdownInt
+  {
+    internal LocalizedString ButtonText;
+    internal Action OnClick;
+
+    internal static UISettingsEntityDropdownButton Create(
+      LocalizedString description, LocalizedString longDescription, LocalizedString buttonText, Action onClick)
+    {
+      var button = ScriptableObject.CreateInstance<UISettingsEntityDropdownButton>();
+      button.m_Description = description;
+      button.m_TooltipDescription = longDescription;
+
+      button.ButtonText = buttonText;
+      button.OnClick = onClick;
+      return button;
+    }
+
+    public override SettingsListItemType? Type => SettingsListItemType.Custom;
+  }
+
+  internal class SettingsEntityDropdownButtonVM : SettingsEntityDropdownVM
+  {
+    private readonly UISettingsEntityDropdownButton buttonEntity;
+
+    public string Text => buttonEntity.ButtonText;
+
+    internal SettingsEntityDropdownButtonVM(UISettingsEntityDropdownButton buttonEntity) : base(buttonEntity)
+    {
+      this.buttonEntity = buttonEntity;
+    }
+
+    public void PerformClick()
+    {
+      buttonEntity.OnClick?.Invoke();
+    }
+  }
+
+  internal class SettingsEntityDropdownButtonView
+    : SettingsEntityDropdownPCView, IPointerEnterHandler, IPointerExitHandler
+  {
+    private SettingsEntityDropdownButtonVM VM => ViewModel as SettingsEntityDropdownButtonVM;
+
+    public override VirtualListLayoutElementSettings LayoutSettings
+    {
+      get
+      {
+        bool set_mOverrideType = m_LayoutSettings == null;
+        m_LayoutSettings ??= new();
+        if (set_mOverrideType)
+        {
+          SettingsEntityPatches.OverrideType.SetValue(
+            m_LayoutSettings, VirtualListLayoutElementSettings.LayoutOverrideType.UnityLayout);
+        }
+
+        return m_LayoutSettings;
+      }
+    }
+
+    private VirtualListLayoutElementSettings m_LayoutSettings;
+
+    public override void BindViewImplementation()
+    {
+      Title.text = ViewModel.Title;
+      ButtonLabel.text = VM.Text;
+      Button.OnLeftClick.RemoveAllListeners();
+      Button.OnLeftClick.AddListener(() =>
+      {
+        VM.PerformClick();
+      });
+
+      SetupColor(isHighlighted: false);
+    }
+
+    private Color NormalColor = Color.clear;
+    private Color HighlightedColor = new(0.52f, 0.52f, 0.52f, 0.29f);
+
+    // These must be public or they'll be null
+    public Image HighlightedImage;
+    public TextMeshProUGUI Title;
+    public OwlcatButton Button;
+    public TextMeshProUGUI ButtonLabel;
+
+    private void SetupColor(bool isHighlighted)
+    {
+      if (HighlightedImage != null)
+      {
+        HighlightedImage.color = isHighlighted ? HighlightedColor : NormalColor;
+      }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+      EventBus.RaiseEvent(delegate (ISettingsDescriptionUIHandler h)
+      {
+        h.HandleShowSettingsDescription(ViewModel.Title, ViewModel.Description);
+      },
+      true);
+      SetupColor(isHighlighted: true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+      EventBus.RaiseEvent(delegate (ISettingsDescriptionUIHandler h)
+      {
+        h.HandleHideSettingsDescription();
+      },
+      true);
+      SetupColor(isHighlighted: false);
+    }
+  }
+}
