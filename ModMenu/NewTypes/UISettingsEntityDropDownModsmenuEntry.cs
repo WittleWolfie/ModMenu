84ca27ab9e1cee0c4351b6a7de2a4b9a61dcb204
ScaredKurufinve <Kaymarciy@gmail.com>
2023-03-07 01:55:23 +0300

    New UI Settings Dropdown - the main feature of the PR. I did not add any new VM and expect it to work correctly with the default dropdown.

---
+++ ModMenu/NewTypes/UISettingsEntityDropDownModsmenuEntry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingmaker.UI.SettingsUI;
+using ModMenu.Settings;
+using UnityEngine;
+
+namespace ModMenu.NewTypes
+{
+  internal class UISettingsEntityDropdownModsmenuEntry : UISettingsEntityDropdown<ModsMenuEntry>
+  {
+    static UISettingsEntityDropdownModsmenuEntry()
+    {
+      instance.LinkSetting(SettingsEntityModsmenuEntry.instance);
+    }
+    internal static UISettingsEntityDropdownModsmenuEntry instance = ScriptableObject.CreateInstance<UISettingsEntityDropdownModsmenuEntry>();
+    public override List<string> LocalizedValues
+    {
+      get
+      { 
+        return ModsMenuEntity.ModEntries.Select(entry => entry.ModInfo.GenerateName()).ToList();
+      }
+    }
+
+    public override int GetIndexTempValue()
+    {
+      return ModsMenuEntity.ModEntries.IndexOf(Setting.GetTempValue());
+    }
+
+
+    public override void SetIndexTempValue(int value)
+    {
+      if (value is < 0 && value > ModsMenuEntity.ModEntries.Count())
+      {
+        Main.Logger.Error($"Value {value} is given to UISettingsEntityDropdownModsmenuEntry when there're only {ModsMenuEntity.ModEntries.Count()} entries in the list");
+        SetTempValue(ModsMenuEntity.ModEntries[0]);
+      }
+
+      SetTempValue(ModsMenuEntity.ModEntries[value]);
+    }
+
+  }
+}
