8c77f2a93044cbdd6a5e7ccc669d538d2eb810c8
ScaredKurufinve <Kaymarciy@gmail.com>
2023-03-07 01:48:17 +0300

    New Setting Entity class with a ModsmenuEntry as a possible setting - on value changed redraws the setting screen to display settings of the chosen mod.

---
+++ ModMenu/NewTypes/SettingsEntityModsmenuEntry.cs
@@ -0,0 +1,18 @@
+﻿using Kingmaker.Settings;
+using ModMenu.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModMenu.NewTypes
+{
+  internal class SettingsEntityModsmenuEntry : SettingsEntity<ModsMenuEntry>
+  {
+    internal static SettingsEntityModsmenuEntry instance = new("modsmenu.entrystaticinstance", ModsMenuEntry.EmptyInstance);
+
+    public SettingsEntityModsmenuEntry(string key, ModsMenuEntry defaultValue) : base(key, defaultValue, false, false, false) 
+      => OnTempValueChanged += new(() => { ModsMenuEntity.settingVM.SwitchSettingsScreen(ModsMenuEntity.SettingsScreenId); return true; });
+  }
+}
