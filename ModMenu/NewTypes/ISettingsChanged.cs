b482b6c1c33c7f991197082194b80547a7503de5
thehambeard <61787444+thehambeard@users.noreply.github.com>
2023-08-29 01:22:44 -0400

    Updated with XML doc comments and added usages in TestSettings.cs

--- ModMenu/NewTypes/ISettingsChanged.cs
+++ ModMenu/NewTypes/ISettingsChanged.cs
@@ -2,9 +2,18 @@
 
 namespace ModMenu.NewTypes
 {
-  // Event interface that can be subscribed to in PubSubSytem that handles when Apply Settings happens in the Settings UI
+  /// <summary>
+  /// Event interface that can be subscribed to on the EventBus that handles when Apply button is pressed in the Settings UI.
+  /// </summary>
+  /// <remarks>
+  /// Your class must subscribe to the EventBus for this to trigger via EventBus.Subscribe(object subscriber)
+  /// </remarks>
+ 
   public interface ISettingsChanged : ISubscriber, IGlobalSubscriber
   {
+    /// <summary>
+    /// Method triggered when with SettingsVM.ApplySettings() is called.
+    /// </summary>
     void HandleApplySettings();
   }
 }
