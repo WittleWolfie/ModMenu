70367f0854e8ea0eb41db69f85d016b97ea5a226
thehambeard <61787444+thehambeard@users.noreply.github.com>
2023-08-28 23:07:39 -0400

    Dynamic descriptions, header fix, and new event
    **Helper SettingsDescriptionUpdater will update a settings long description dynamically.
    **An event is trigger when the Settings UI is opened to ensure the scale is 1. This fixes the header button being too big in the in game settings menu.
    **New PubSubSystem interface that triggers when the Settings UI calls ApplySettings

---
+++ ModMenu/NewTypes/ISettingsChanged.cs
@@ -0,0 +1,10 @@
+﻿using Kingmaker.PubSubSystem;
+
+namespace ModMenu.NewTypes
+{
+  // Event interface that can be subscribed to in PubSubSytem that handles when Apply Settings happens in the Settings UI
+  public interface ISettingsChanged : ISubscriber, IGlobalSubscriber
+  {
+    void HandleApplySettings();
+  }
+}
